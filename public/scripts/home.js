60802d2f0e7005494d35efa9a3a9f21949c95bd2
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2017-09-03 15:32:40 -0400

    modularized es5 components

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
-    buildGlobe();
-});
 
-
-function buildGlobe(){
-
-  var start = Date.now(),
+   start = Date.now(),
       speed = 1e-2,
       width = 600,
       height = 500,
@@ -17,22 +12,29 @@ function buildGlobe(){
                         .clipAngle(90)
                         .precision(.5);
     
-var path = d3.geo.path()
+ path = d3.geo.path()
             .projection(projection);
 
-var svg = d3.select("#globeDiv")
+ svg = d3.select("#globeDiv")
             .append("svg")
             .attr("width", "100%")
             .attr("height", "100%")
             .attr("viewBox", "0 0 600 500")
             .attr("preserveAspectRatio", "xMinYMin meet");
+            queueData();
+});
+
+
+
+function queueData(){
 
 queue()
     .defer(d3.json, "https://raw.githubusercontent.com/rekhers/d3_maps/gh-pages/data/world-110m2.json")
     .defer(d3.tsv, "public/data/world-names.tsv")
     .defer(d3.json, "public/data/cities.json")
-    .await(ready);
+    .await(ready)
 
+}
 
 
 
@@ -69,10 +71,6 @@ var countries = topojson.feature(world, world.objects.countries).features,
         cityCoords[cityList[z]] = _.findWhere(cities.features, {id: cityList[z]});
     }
 
-    console.log(cityCoords)
-    console.log(cityCoords);
-
-
   //draw water
  var water = svg.append("path")
                   .datum({type: "Sphere"})
@@ -88,18 +86,23 @@ var countries = topojson.feature(world, world.objects.countries).features,
                     .attr("class", "land")
                     .attr("d", path);
 
+    hover(cityCoords);
 
-  
+}
+
+
+
+
+
+
+function hover(cityCoords){
 
 $("#listDiv div").hover(function() {
 
-    if(currCity){
+  if(currCity){
     var lastCity = currCity;
-    console.log(currCity);
   }
     var currCity = $(this).attr('data-id');
-     console.log(currCity);
-
 
     //for London
     var UK = _.findWhere(countriesWithNames, {name: "United Kingdom"});
@@ -127,7 +130,7 @@ $("#listDiv div").hover(function() {
 
 
  //how we're going to add points 
-  svg.insert("path")
+  svg.append("path")
     .datum({type: "Point", coordinates: [cityCoords[currCity].geometry.coordinates[0], cityCoords[currCity].geometry.coordinates[1]]})
     .attr("class", "points")
     .attr("d", path)
@@ -135,14 +138,30 @@ $("#listDiv div").hover(function() {
     .attr("stroke-width", 25);
 
 
+
+  var plane = svg.append("path")
+                   .attr("class", "plane")
+                   .attr("d", "m25.21488,3.93375c-0.44355,0 -0.84275,0.18332 -1.17933,0.51592c-0.33397,0.33267 -0.61055,0.80884 -0.84275,1.40377c-0.45922,1.18911 -0.74362,2.85964 -0.89755,4.86085c-0.15655,1.99729 -0.18263,4.32223 -0.11741,6.81118c-5.51835,2.26427 -16.7116,6.93857 -17.60916,7.98223c-1.19759,1.38937 -0.81143,2.98095 -0.32874,4.03902l18.39971,-3.74549c0.38616,4.88048 0.94192,9.7138 1.42461,13.50099c-1.80032,0.52703 -5.1609,1.56679 -5.85232,2.21255c-0.95496,0.88711 -0.95496,3.75718 -0.95496,3.75718l7.53,-0.61316c0.17743,1.23545 0.28701,1.95767 0.28701,1.95767l0.01304,0.06557l0.06002,0l0.13829,0l0.0574,0l0.01043,-0.06557c0,0 0.11218,-0.72222 0.28961,-1.95767l7.53164,0.61316c0,0 0,-2.87006 -0.95496,-3.75718c-0.69044,-0.64577 -4.05363,-1.68813 -5.85133,-2.21516c0.48009,-3.77545 1.03061,-8.58921 1.42198,-13.45404l18.18207,3.70115c0.48009,-1.05806 0.86881,-2.64965 -0.32617,-4.03902c-0.88969,-1.03062 -11.81147,-5.60054 -17.39409,-7.89352c0.06524,-2.52287 0.04175,-4.88024 -0.1148,-6.89989l0,-0.00476c-0.15655,-1.99844 -0.44094,-3.6683 -0.90277,-4.8561c-0.22699,-0.59493 -0.50356,-1.07111 -0.83754,-1.40377c-0.33658,-0.3326 -0.73578,-0.51592 -1.18194,-0.51592l0,0l-0.00001,0l0,0z");
+
+    var route = svg.append("path")
+      .datum({type: "LineString", coordinates: [72, -126] })
+        .attr("class", "route")
+         .attr("d", path);
+
 if(lastCity){
 
-    svg.insert("path")
+    svg.append("path")
     .datum({type: "Point", coordinates: [cityCoords[lastCity].geometry.coordinates[0], cityCoords[lastCity].geometry.coordinates[1]]})
     .attr("class", "points")
     .attr("d", path)
     .style("fill", "red")
     .attr("stroke-width", 25);
+
+    // var route = svg.append("path")
+    //            .datum({type: "LineString", coordinates: [[cityCoords[lastCity].geometry.coordinates[0], cityCoords[lastCity].geometry.coordinates[1]], [cityCoords[currCity].geometry.coordinates[0], cityCoords[currCity].geometry.coordinates[1]]]})
+    //            .attr("class", "route")
+    //            .attr("d", path);
+
 
 }
     var rotate = projection.rotate();
@@ -163,8 +182,4 @@ if(lastCity){
 
 
 
-
-
 }
-}
-
