92425e41ad8f40b27d8699b1d87e57a94a54d2f3
Rekha Tenjarla <rtenjarla@theatlantic.com>
2018-11-29 21:34:24 -0500

    update deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,21 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        // running `grunt less` will compile once
-        less: {
-            development: {
-                options: {
-                    paths: ["./public/stylesheets"],
-                    yuicompress: true
-                },
-            files: {
-                "./public/stylesheets/home.css": "./public/stylesheets/home.less"
-            }
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          paths: ["./public/stylesheets"],
+          yuicompress: true
+        },
+        files: {
+          "./public/stylesheets/home.css": "./public/stylesheets/home.less"
         }
+      }
     },
-    // running `grunt watch` will watch for changes
     watch: {
-        files: "./public/stylesheets/*.less",
-        tasks: ["less"]
+      files: "./public/stylesheets/*.less",
+      tasks: ["less"]
     }
-});
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  });
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 };
